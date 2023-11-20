d35756bd84d182698908cc68d5aeffb09982dee4
E.Wolf <e.wolf@webmi.ru>
2023-11-21 01:02:21 +0300

    Project init / draft code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Speech.Synthesis;
+
+var synth = new SpeechSynthesizer();
+var voices = synth.GetInstalledVoices();
+
+Console.WriteLine("Available voices:");
+for (var i = 0; i < voices.Count; i++)
+{
+    Console.Write(i + 1);
+    Console.Write(". ");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write(voices[i].VoiceInfo.Name);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(" [");
+    Console.Write(voices[i].VoiceInfo.Culture.DisplayName);
+    Console.WriteLine("]");
+    Console.ResetColor();
+}
+
+synth.SelectVoice("IVONA 2 Maxim OEM");
+synth.Speak("Я программа говорильня, привет!");
+Console.ReadKey();
